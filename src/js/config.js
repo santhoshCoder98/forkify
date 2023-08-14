40585328084867bf4bc038ff26b99edec8ac64fc
Santhosh Chandrasekaran <42298132+worstCoder@users.noreply.github.com>
2023-08-14 13:52:11 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '9ad50374-94cf-4543-8ae1-7c2e7c1d3089';
+export const KEY = 'GENERATE_API_KEY_URL_https://forkify-api.herokuapp.com/v2';
 export const MODAL_CLOSE_SEC = 2.5;
